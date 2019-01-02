66eb1daa4dd599bcad88e6cdacd01e42cc521581
Paulo Aguiar <pauloaguiars2cg@gmail.com>
2019-01-02 18:38:00 +0000

    trying stuff

--- myguide/app/Http/Controllers/HomeController.php
+++ myguide/app/Http/Controllers/HomeController.php
@@ -19,44 +19,51 @@ class HomeController extends Controller
         $user = Auth::user();
         $now = new DateTime();
 
-        $getUserEvents = DB::table('users_events')->select('event_id')
-                        ->where('user_id','=', $user->id)->distinct()
-                        ->pluck('event_id');
+        if($user){
+            $getUserEvents = DB::table('users_events')->select('event_id')
+                            ->where('user_id','=', $user->id)->distinct()
+                            ->pluck('event_id');
+        
+            $adminevents = DB::table('events')
+                        ->where('date','>', $now)
+                        ->get();
+                            
+            $alleventsID = DB::table('events')->select('id')
+                        ->where('date','>', $now)
+                        ->pluck('id');
 
-        $adminevents = DB::table('events')
-                    ->where('date','>', $now)
-                    ->get();
-						
-		$alleventsID = DB::table('events')->select('id')
-					->where('date','>', $now)
-					->pluck('id');
-
-		$current_pax = array();
-		foreach ($alleventsID as $value) {
-			$query = DB::table('users_events')
-                    ->where('event_id','=', $value)
-					->get();
-			array_push($current_pax, $query);
-		}
-
-        $oldevents = array();
-        $newevents = array();
-        foreach ($getUserEvents as $value) {
-            $query = DB::table('events')
-                    ->where('id','=', $value)
-                    ->get();
-            $querydate = DB::table('events')->select('date')
-                        ->where('id','=', $value)
-						->pluck('date');			
-			$dateQUERY = new DateTime($querydate[0]);
-            if ($dateQUERY < $now) {
-                array_push($oldevents, ...$query);
+            $current_pax = array();
+            foreach ($alleventsID as $value) {
+                $query = DB::table('users_events')
+                        ->where('event_id','=', $value)
+                        ->get();
+                array_push($current_pax, $query);
             }
-            else
-            {
-                array_push($newevents, ...$query);
-			} 			
-		}
-        return view('home', ['oldevents' => $oldevents, 'newevents' => $newevents, 'adminevents' => $adminevents, 'current_pax' => $current_pax]);
+
+            $oldevents = array();
+            $newevents = array();
+            
+            foreach ($getUserEvents as $value) {
+                $query = DB::table('events')
+                        ->where('id','=', $value)
+                        ->get();
+                $querydate = DB::table('events')->select('date')
+                            ->where('id','=', $value)
+                            ->pluck('date');			
+                $dateQUERY = new DateTime($querydate[0]);
+                if ($dateQUERY < $now) {
+                    array_push($oldevents, ...$query);
+                }
+                else
+                {
+                    array_push($newevents, ...$query);
+                } 			
+            }
+            return view('home', ['oldevents' => $oldevents, 'newevents' => $newevents, 'adminevents' => $adminevents, 'current_pax' => $current_pax]);
+        }
+        else
+        {
+            return view('aboutus');
+        }
     }
 }
